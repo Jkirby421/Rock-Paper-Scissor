d23647900078f4a780f8968264877d08fa93eabd
Jkirby421 <jormalkirby@gmail.com>
2023-08-27 18:47:14 -0400

    Added UI to rps

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+const options = document.querySelectorAll(".choice");
+const notice = document.querySelector("#alert");
+const winMessage = "You Win!";
+const loseMessage = "You Lose :(";
+const tieMessage = "TIE!";
+
+let playerScore = document.querySelector('.playerScore');
+let computerScore = document.querySelector('.computerScore');
+
+
+let yourScore = 0;
+let compScore = 0;
+
+playerScore.textContent = playerScore.textContent + " " + yourScore.toString();
+computerScore.textContent = "Computer Score: " + compScore.toString();
+
+
+
+options.forEach(option => option.addEventListener('click', () => {
+choice = option.textContent;
+(playRound(choice));
+}));
+
+
+
 function getComputerChoice(){
     let number = Math.floor(Math.random() * (4-1)) + 1;
     if (number === 3){
@@ -10,70 +35,79 @@ function getComputerChoice(){
         return "scissors";
     }
     }
+
+    function playerWin(){
+        notice.textContent = winMessage;
+        playerScore.textContent = "Your Score: " + yourScore.toString();
+        options.forEach(option => option.disabled = true);
+        
+    }
+
+    function computerWin(){
+        notice.textContent = loseMessage;
+        computerScore.textContent = "Computer Score: " + compScore.toString();
+        options.forEach(option => option.disabled = true);
+    }
+
+    function tryAgain() {
+        const tryButton = document.createElement('button');
+        tryButton.innerText = "Try Again";
+        tryButton.addEventListener('click', () => {
+            notice.textContent = "";
+            yourScore = 0;
+            compScore = 0;
+            playerScore.textContent = "Your Score: " + yourScore.toString();
+            computerScore.textContent = "Your Score: " + compScore.toString();
+            options.forEach(option => option.disabled = false);
+            document.body.removeChild(tryButton);
+        });
+        document.body.appendChild(tryButton);
+    }
     
-    function playRound(){
-        let playerSelection = prompt("Rock, Paper, or Scissor?");
+    function playRound(playerSelection){
         playerSelection = playerSelection.toLowerCase();
         let computerSelection = getComputerChoice();
-        console.log("Computer:"+computerSelection);
-        console.log("Player:"+playerSelection);
+        notice.textContent = ("");
     
         if (playerSelection === computerSelection){
-            return "tie!";
+            notice.textContent = tieMessage;
+            console.log(notice.classList)
         }
         else if (playerSelection === "rock"){
-            if (computerSelection === "scissor"){
-                return "you win";
+            if (computerSelection === "scissors"){
+                yourScore++;
             }
             else {
-                return "you lose";
+                compScore++;
             }
         }
         else if (playerSelection === "paper"){
             if (computerSelection === "rock") {
-                return "you win";
+                yourScore++;
             }
             else {
-                return "you lose";
+                compScore++;
             }
         }
-        else if (playerSelection === "scissors"){
-                if (computerSelection === "rock"){
-                    return "you win";
+        else {
+                if (computerSelection === "paper"){
+                    yourScore++;
                 }
                 else {
-                    return "you lose";
+                    compScore++;
                 }
             }
+            playerScore.textContent = "Your Score: " + yourScore.toString();
+            computerScore.textContent = "Computer Score: " + compScore.toString();
+
+        if (yourScore === 5) {
+            playerWin();
+            tryAgain();
+        };
+        if(compScore === 5) {
+            computerWin();
+            tryAgain();
+        } 
     
-        else {
-            return "Please enter a valid option";
-        }
-    }
-    
-    function game(){
-        let winCount = 0;
-        let loseCount = 0;
-        let roundCounter = 1;
-    
-        while (roundCounter <= 5){
-        console.log("Rounds " + roundCounter);
-        let round = playRound();
-        if (round === "you win"){
-            winCount++;
-            roundCounter++;
-        }
-        else if (round === "you lose"){
-            loseCount++;
-            roundCounter++;  
-        }
-        else if (round === "tie"){
-        continue;
-        }
-        else{
-            console.log("Please enter a valid option");
-        }
-        }
-        roundcounter = 0;
     }
     
